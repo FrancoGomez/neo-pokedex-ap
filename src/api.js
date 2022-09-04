7d6d3595b95371e31802e48ddab9a8d6705c61c3
FrancoGomez <mgomez21699@gmail.com>
2022-09-04 17:55:12 -0300

    Refactor all the external calls into a diferent js file

---
+++ src/api.js
@@ -0,0 +1,29 @@
+export const getPokemonList = async (pageNumber) => {
+    const POKEMON_PER_PAGE = 20;
+
+    const pokemonList = await fetch(
+        `https://pokeapi.co/api/v2/pokemon?offset=${
+            POKEMON_PER_PAGE * pageNumber
+        }&limit=${POKEMON_PER_PAGE}`
+    );
+
+    if (pokemonList.status === 200) {
+        return pokemonList.json();
+    }
+
+    return;
+};
+
+export const getPokemonInfo = async (id) => {
+    const pokemonInfo = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+    if (pokemonInfo.status === 200) {
+        return pokemonInfo.json();
+    }
+
+    return;
+};
+
+export const getPokemonImage = (id) => {
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+};
