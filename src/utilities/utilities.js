a4ee52d2724dffe1a46fa55e60684e7c4e049c4d
FrancoGomez <mgomez21699@gmail.com>
2022-09-09 14:17:51 -0300

    Refactor index.js passing utility functions

---
+++ src/utilities/utilities.js
@@ -0,0 +1,11 @@
+export const toggleDisable = ($element) => {
+    $element.classList.toggle("disabled");
+};
+
+export const toggleActive = ($element) => {
+    $element.classList.toggle("active");
+};
+
+export const toggleHidden = ($element) => {
+    $element.classList.toggle("hidden");
+};
