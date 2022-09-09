cb47fc24960b6ebc616184dd56ec373e15a5b596
FrancoGomez <mgomez21699@gmail.com>
2022-09-09 14:34:09 -0300

    Fix error that defined paginator code inside navigation and define the paginator onclick function

--- src/ui/paginator.js
+++ src/ui/paginator.js
@@ -1,6 +1,60 @@
 import { toggleDisable, toggleActive } from "../utilities/utilities.js";
 import { deletePokemonCardsContainer, createPokemonCards } from "./cards.js";
 
+export const createPageNavigationOptions = () => {
+    const POKEMON_COUNT = 898;
+    const POKEMON_PER_PAGE = 20;
+    const PAGE_AMOUNT = Math.ceil(POKEMON_COUNT / POKEMON_PER_PAGE);
+
+    const $pagination = document.querySelector("#pagination");
+
+    $pagination.onclick = (e) => {
+        handlePaginationClick(e.target);
+    };
+
+    const $fragment = document.createDocumentFragment();
+
+    const $pagePreviousOption = createNavigationOption("Previous");
+    $fragment.appendChild($pagePreviousOption);
+
+    for (let i = 1; i <= PAGE_AMOUNT; i++) {
+        const $pageOption = createNavigationOption(i);
+        $fragment.appendChild($pageOption);
+    }
+
+    const $pageNextOption = createNavigationOption("Next");
+    $fragment.appendChild($pageNextOption);
+
+    $pagination.appendChild($fragment);
+};
+
+const createNavigationOption = (textContent) => {
+    const $pageOption = document.createElement("li");
+    $pageOption.className = "page-item page-navigation-option";
+    $pageOption.setAttribute("id", `button-${textContent}`);
+
+    if (textContent === "Previous") {
+        $pageOption.classList.add("disabled");
+    }
+
+    if (typeof textContent === "number") {
+        $pageOption.classList.add("option");
+
+        if (textContent === 1) {
+            $pageOption.classList.add("active");
+        }
+    }
+
+    const $pageLinkOption = document.createElement("a");
+    $pageLinkOption.className = "page-link";
+
+    $pageLinkOption.textContent = textContent;
+
+    $pageOption.appendChild($pageLinkOption);
+
+    return $pageOption;
+};
+
 export const handlePaginationClick = (button) => {
     if (
         button.classList.contains("disabled") ||
